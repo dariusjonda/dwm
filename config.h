bd1ef08e7d03a1aa5565fc6cd6ca749ce06b149b
dariusjonda <darius.jonda@transdev.de>
2021-03-27 08:50:19 +0100

    basic conf changes

--- config.h
+++ config.h
@@ -8,6 +8,11 @@ static const int showbar              = 1;        /* 0 means no bar */
 static const int topbar               = 1;        /* 0 means bottom bar */
 static const char *fonts[]            = { "FiraCode-Regular:size=15" };
 static const char dmenufont[]         = "FiraCode-Regular:size=15";
+static const char col_gray1[]         = "#222222";
+static const char col_gray2[]         = "#444444";
+static const char col_gray3[]         = "#bbbbbb";
+static const char col_gray4[]         = "#eeeeee";
+static const char col_cyan[]          = "#005577";
 static const char col_dark_grey[]     = "#030303";
 static const char col_red[]           = "#ff009e";
 static const char col_white[]         = "#ffffff";
@@ -67,7 +72,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]              = { "st", "-n", "popterm", "-e", "decency_fzf_run", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]               = { "st", NULL };
 static const char *poptermcmd[]            = { "st", "-n", "popterm", "-g", "90x30", NULL };
 static const char *scrupscreencmd[]        = { "scrup_screen", "1", NULL };
