ec878a1b0ed02afd008f9a941bed79299a470957
dariusjonda <darius.jonda@transdev.de>
2021-03-27 08:41:43 +0100

    custom changes to config.h

--- config.h
+++ config.h
@@ -4,10 +4,10 @@
 static const unsigned int borderpx    = 3;        /* border pixel of windows */
 static const int gappx                = 10;        /* gaps between windows */
 static const unsigned int snap        = 0;        /* snap pixel */
-static const int showbar              = 0;        /* 0 means no bar */
+static const int showbar              = 1;        /* 0 means no bar */
 static const int topbar               = 1;        /* 0 means bottom bar */
-static const char *fonts[]            = { "IBM Plex Mono:size=11" };
-static const char dmenufont[]         = "IBM Plex Mono:size=11";
+static const char *fonts[]            = { "FiraCode-Regular:size=15" };
+static const char dmenufont[]         = "FiraCode-Regular:size=15";
 static const char col_dark_grey[]     = "#030303";
 static const char col_red[]           = "#ff009e";
 static const char col_white[]         = "#ffffff";
@@ -18,7 +18,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "main", "code", "book", "misc" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -55,7 +55,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
