de3a2b7adef6924335c5104dec80f4e7c0e045f9
dariusjonda <darius.jonda@transdev.de>
2021-03-27 11:26:45 +0100

    changes commited

--- config.h
+++ config.h
@@ -4,13 +4,13 @@
 static const unsigned int borderpx    = 2;        /* border pixel of windows */
 static const int gappx                = 3;        /* gaps between windows */
 static const unsigned int snap        = 0;        /* snap pixel */
-static const int showbar              = 1;        /* 0 means no bar */
+static const int showbar              = 0;        /* 0 means no bar */
 static const int topbar               = 1;        /* 0 means bottom bar */
 static const char *fonts[]            = { "FiraCode-Regular:size=15" };
 static const char dmenufont[]         = "FiraCode-Regular:size=15";
 static const char col_cyan[]          = "#005577";
 static const char col_dark_grey[]     = "#030303";
-static const char col_red[]           = "#ff009e";
+static const char col_red[]           = "#3ae02b";
 static const char col_white[]         = "#ffffff";
 static const char *colors[][3]      = {
 	/*               fg                  bg                   border   */
@@ -68,9 +68,6 @@ static const char *browsercmd[]            = { "firefox", NULL };
 static const char *filemanagercmd[]        = { "st", "-e", "ranger", NULL };
 static const char *clipboardcmd[]          = { "clipmenu", NULL };
 static const char *colourpickercmd[]       = { "gcolor2", NULL };
-static const char *togglecompositorcmd[]   = { "togglecompositor", NULL };
-static const char *toggleresolutioncmd[]   = { "toggleresolution", NULL };
-static const char *togglemonitorinputcmd[] = { "togglemonitorinput", NULL };
 
 #include "movestack.c"
 #include <X11/XF86keysym.h>
@@ -85,9 +82,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_r,                       spawn,          {.v = filemanagercmd } },
 	{ MODKEY,                       XK_v,                       spawn,          {.v = clipboardcmd } },
 	{ MODKEY,                       XK_z,                       spawn,          {.v = colourpickercmd } },
-	{ MODKEY|ShiftMask,             XK_c,                       spawn,          {.v = togglecompositorcmd } },
-	{ MODKEY|ControlMask,           XK_c,                       spawn,          {.v = toggleresolutioncmd } },
-	{ MODKEY|ShiftMask,             XK_m,                       spawn,          {.v = togglemonitorinputcmd } },
 	{ MODKEY,                       XK_b,                       togglebar,      {0} },
 	{ MODKEY,                       XK_j,                       focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,                       focusstack,     {.i = -1 } },
@@ -95,19 +89,19 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,                       movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_i,                       incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,                       incnmaster,     {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_h,                       setmfact,       {.f = -0.001} },
-	{ MODKEY|ControlMask,           XK_l,                       setmfact,       {.f = +0.001} },
+	{ MODKEY,                       XK_h,                       setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,                       setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return,                  zoom,           {0} },
 	{ MODKEY,                       XK_Escape,                  view,           {0} },
-	{ MODKEY,                       XK_w,                       killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_c,                       killclient,     {0} },
 	{ MODKEY,                       XK_m,                       setlayout,      {0} },
 	{ MODKEY,                       XK_0,                       view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,                       tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_h,                       focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_l,                       focusmon,       {.i = +1 } },
+	{ MODKEY,                       XK_comma,                   focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,                  focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_h,                       tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_l,                       tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_period,                  setcentered,    {0} },
+	{ MODKEY|ShiftMask,             XK_period,                  setcentered,    {0} },
 	TAGKEYS(                        XK_1,                                       0)
 	TAGKEYS(                        XK_2,                                       1)
 	TAGKEYS(                        XK_3,                                       2)
