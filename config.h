1d64d5e0449c8ae33728e35203213d04c85367f1
dariusjonda <darius.jonda@transdev.de>
2021-03-27 08:59:17 +0100

    minor config.h changes

--- config.h
+++ config.h
@@ -72,7 +72,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]               = { "st", NULL };
 static const char *poptermcmd[]            = { "st", "-n", "popterm", "-g", "90x30", NULL };
 static const char *scrupscreencmd[]        = { "scrup_screen", "1", NULL };
